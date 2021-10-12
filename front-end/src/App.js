de154226d69cc3de543844cf6e101ca2a99a7801
Dennis Kenneth <dennyify123@gmail.com>
2021-10-12 13:16:23 +0000

    Home Screen Component created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,20 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
   return (
     <div className='App'>
       <Header />
+
+      {/* BODY AREA */}
       <main className='py-3'>
         <Container>
-          <h1>Welcome To Dark Secret fragrance</h1>
+          {/* <h1>Welcome To Dark Secret fragrance</h1> */}
+          <HomeScreen />
         </Container>
       </main>
+
       <Footer />
     </div>
   );
---
+++ front-end/src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import products from "../products";
+
+// const fxn = (element) => {
+//     return <Col>{element</Col>
+// }
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {/* eslint-disable-next-line */}
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
