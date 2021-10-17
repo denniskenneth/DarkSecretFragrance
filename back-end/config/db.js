9208de849144536d5a9a1cf49164bd53ea6760bc
Dennis Kenneth <dennyify123@gmail.com>
2021-10-17 20:40:46 +0000

    data model schema created

---
+++ back-end/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const DB_URI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      //   useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
