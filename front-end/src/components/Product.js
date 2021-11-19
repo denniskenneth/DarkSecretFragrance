56fd65c3fe00839b589cfff4f11415e4620ca499
Dennis Kenneth <dennyify123@gmail.com>
2021-11-19 21:02:36 +0000

    rating stars added

--- front-end/src/components/Product.js
+++ front-end/src/components/Product.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
@@ -17,12 +18,13 @@ const Product = ({ product }) => {
         </Link>
 
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
 
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as='h3'>${product.price} </Card.Text>
       </Card.Body>
     </Card>
   );
