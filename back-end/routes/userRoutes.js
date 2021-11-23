80d12e098aae846cf43ac2ce588d9b734d28a7aa
Dennis Kenneth <dennyify123@gmail.com>
2021-11-23 23:17:38 +0000

    custom auth middleware -- created

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 
-import { authUser } from '../controllers/userController.js';
-
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 //PURPOSE:  FETCH ALL PRODUCT
 // ROUTE: GET/api/products
 // ACCESS: public
 router.post('/login', authUser);
+
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
