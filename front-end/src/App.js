ed5a3c7689bba5bf88a445076ed64d5728aa5069
Dennis Kenneth <dennyify123@gmail.com>
2021-11-26 23:23:57 +0000

    created user edit screen, get user detaails applied

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import UserListScreen from './screens/UserLIstScreen';
+import UserEditScreen from './screens/UserEditScreen';
 const App = () => {
   return (
     <Router className='App'>
@@ -26,6 +27,7 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userList' component={UserListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
         </Container>
       </main>
 
