a6f5597402d411233a373862bd49e2bc3bfcc8b5
Dennis Kenneth <dennyify123@gmail.com>
2021-11-26 07:47:33 +0000

    user login reducers created && user login actions created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/CartScreen";
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import ShippingScreen from './screens/ShippingScreen';
 const App = () => {
   return (
     <Router className='App'>
@@ -13,6 +14,7 @@ const App = () => {
       {/* BODY AREA */}
       <main className='py-3'>
         <Container>
+          <Route path='/shipping' component={ShippingScreen} exact />
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
