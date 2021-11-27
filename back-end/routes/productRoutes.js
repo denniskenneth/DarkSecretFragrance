cf222fec8414ef9eda3f7f4e394038d4a415667c
Dennis Kenneth <dennyify123@gmail.com>
2021-11-27 11:03:09 +0000

    Endpoints creaated: Create Product && Update Product

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -3,6 +3,8 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -11,7 +13,11 @@ const router = express.Router();
 //PURPOSE:  FETCH ALL PRODUCT
 // ROUTE: GET/api/products
 // ACCESS: public
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
